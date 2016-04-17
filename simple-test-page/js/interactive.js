b9aa2d1e3c28ea578d7b69a339708720f98413a1
Brendan Spijkerman <brendan@spykerman.nl>
2016-04-17 23:55:30 +0200

    Improved the test page and added a gulp task for it

--- simple-test-page/js/interactive.js
+++ simple-test-page/js/interactive.js
@@ -1,72 +1,81 @@
 var particle = new Particle();
 var token
+var payload = ''
 
 particle.login({username: $('#username').val(), password: $('#password').val()}).then( function(result) {
 
 	console.log(result)
-	token = result.body.access_token;
+	token = result.body.access_token
 
 }).catch( function(err) {
 
-	window.alert('Something went wrong')
+	// window.alert('Something went wrong')
+	console.log('Something went wrong')
 
 })
 
 $('#send').on('click tap', function() {
-	var arguments = 'c';
-	var values = [];
-	var i = 0;
 
-	$('.color').each(function(obj) {
+	encodeData()
 
-		if (i % 2 == 0) {
+	var deviceId = $('#deviceid').val()
 
-			values[i] = Math.floor(parseInt($(obj).val()) / 128);
+	var fnPr = particle.callFunction({ deviceId: deviceId, name: 'lights', argument: payload, auth: token })
 
-		} else {
+	fnPr.then(
 
-			values[i] = parseInt($(obj).val()) - Math.floor(parseInt($(obj).val()) / 128);
-		}
+		function(data) {
+
+			console.log('Function called succesfully:', data)
+
+		}, function(err) {
+
+			console.log('An error occurred:', err)
 
 	})
 
-	console.log(values);
+})
 
-	// Set all channels to 'on'
-	arguments += String.fromCharCode(127);
+$('#encode-data').on('click tap', function() {
 
-	arguments += String.fromCharCode(Math.floor($('#c0r').val() / 128));
-	arguments += String.fromCharCode(Math.floor($('#c0r').val()) - Math.floor($('#c0r').val() / 128));
-
-	arguments += String.fromCharCode(Math.floor($('#c0g').val() / 128));
-	arguments += String.fromCharCode(Math.floor($('#c0g').val()) - Math.floor($('#c0r').val() / 128));
-
-	arguments += String.fromCharCode(Math.floor($('#c0b').val() / 128));
-	arguments += String.fromCharCode(Math.floor($('#c0b').val()) - Math.floor($('#c0r').val() / 128));
-
-	arguments += String.fromCharCode(Math.floor($('#c1r').val() / 128));
-	arguments += String.fromCharCode(Math.floor($('#c0r').val()) - Math.floor($('#c0r').val() / 128));
-
-	arguments += String.fromCharCode(Math.floor($('#c1g').val() / 128));
-	arguments += String.fromCharCode(Math.floor($('#c0g').val()) - Math.floor($('#c0r').val() / 128));
-
-	arguments += String.fromCharCode(Math.floor($('#c1b').val() / 128));
-	arguments += String.fromCharCode(Math.floor($('#c0b').val()) - Math.floor($('#c0r').val() / 128));
-
-	arguments += String.fromCharCode(1);
-	arguments += String.fromCharCode(1);
-	arguments += String.fromCharCode($('#interpolationtime').val());
-
-	console.log(arguments);
-	console.log(arguments.length);
-
-	var fnPr = particle.callFunction({ deviceId: $('#deviceid').val(), name: 'lights', argument: arguments, auth: token });
-
-	fnPr.then(
-		function(data) {
-			console.log('Function called succesfully:', data);
-		}, function(err) {
-			console.log('An error occurred:', err);
-	});
+	encodeData()
 
 })
+
+function encodeData() {
+
+	payload = 'c'
+
+	payload += String.fromCharCode(127)
+
+	$('.color').each(function(obj) {
+
+		var val = $(this).val()
+
+		// If the value exceeds the maximum allowed, cut off
+		// Since we can't send '0', the range is 127 values per byte (1-127)
+		if (val > 127 * 127) {
+
+			val = 127 * 127
+
+		}
+
+		var highByte = Math.floor(val / 127)
+		var lowByte = val - (highByte * 127)
+
+		console.log('High Byte', highByte)
+		console.log('Low Byte', lowByte)
+
+		payload += String.fromCharCode(highByte)
+		payload += String.fromCharCode(lowByte)
+
+	})
+
+	payload += String.fromCharCode(1)
+	payload += String.fromCharCode(1)
+	payload += String.fromCharCode($('#interpolationtime').val())
+
+	console.log('Payload', payload)
+	console.log('Payload length', payload.length)
+
+}
