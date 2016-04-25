4df23929a497fe8ffde7cdedcd0b66cc86e2dc82
Alexander Weiss <ik@alexanderweiss.nl>
2016-04-25 14:59:27 +0200

    Fixed localStorage check

--- js/setup/LITSetupView.js
+++ js/setup/LITSetupView.js
@@ -11,7 +11,7 @@ class SetupView extends lrs.LRSView.views.PagedView {
 
 		var _this = this
 
-		if (window.localStorage.accessToken.length > 20) {
+		if (window.localStorage.accessToken && window.localStorage.accessToken.length > 20) {
 
 			_this.showView(new lrs.LRSView.views.RoomsView())
 
