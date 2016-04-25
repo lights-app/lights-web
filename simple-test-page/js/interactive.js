791a7521162f38f3c9b6923e6842a8b143511b91
Brendan Spijkerman <brendanspy@gmail.com>
2016-04-25 14:59:33 +0200

    Put light values at the end of the payload to ease possible future channel count increase

--- simple-test-page/js/interactive.js
+++ simple-test-page/js/interactive.js
@@ -16,7 +16,7 @@ if(token === null) {
 
 $('#log-in').on('click tap', function() {
 
-	
+	particleLogin()
 
 })
 
@@ -157,6 +157,20 @@ function encodeData(colors) {
 
 	payload += String.fromCharCode(127)
 
+	var val = $('#interpolationtime').val()
+
+	var highByte = Math.floor(val / (127 * 127)) + 1
+	val = val - ((highByte - 1) * (127 * 127))
+	var highByte2 = Math.floor(val / 127) + 1
+	val = val - ((highByte2 - 1) * 127)
+	var lowByte = val + 1
+
+	console.log("Interpolation values", highByte, highByte2, lowByte)
+
+	payload += String.fromCharCode(highByte)
+	payload += String.fromCharCode(highByte2)
+	payload += String.fromCharCode(lowByte)
+
 	colors.forEach(function(obj) {
 
 		var val = obj
@@ -180,19 +194,7 @@ function encodeData(colors) {
 
 	})
 
-	var val = $('#interpolationtime').val()
-
-	var highByte = Math.floor(val / (127 * 127)) + 1
-	val = val - ((highByte - 1) * (127 * 127))
-	var highByte2 = Math.floor(val / 127) + 1
-	val = val - ((highByte2 - 1) * 127)
-	var lowByte = val + 1
-
-	console.log("Interpolation values", highByte, highByte2, lowByte)
-
-	payload += String.fromCharCode(highByte)
-	payload += String.fromCharCode(highByte2)
-	payload += String.fromCharCode(lowByte)
+	
 
 	console.log('Payload', payload)
 	console.log('Payload length', payload.length)
