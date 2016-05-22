a9587c1ff9b7dcf721f60d5f467c3808f3f71d88
Brendan Spijkerman <brendanspy@gmail.com>
2016-05-22 15:49:41 +0200

    Moved login check to app level

--- js/setup/SetupView.js
+++ js/setup/SetupView.js
@@ -6,22 +6,12 @@ class SetupView extends lrs.LRSView.views.PagedView {
 		
 		super(el, options)
 
-		if (lights.app.particle.isLoggedIn) {
-			
-			// Note: Temporary; this check should happen at the app level not at the setup level.
-			var self = this
-			setTimeout( function() {
-				self.owner.views.rooms.showView(new lrs.LRSView.views.RoomsOverviewView())
-			}, 1)
-
-		}
-
 		return this
 	}
 
 	
 	startAction() {
-		
+
 		this.showView(new lrs.LRSView.views.SetupLoginView())
 		
 	}
