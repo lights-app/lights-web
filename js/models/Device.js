475b9bedc2edf60e59e1e2a36218695096262c64
Brendan Spijkerman <brendanspy@gmail.com>
2016-05-30 00:48:12 +0200

    Added color data encoding (and sending) to devices

--- js/models/Device.js
+++ js/models/Device.js
@@ -33,7 +33,55 @@ class Device extends lrs.LRSObject {
 			this[attributeName] = attributes[attributeName]
 
 		}
+
+		this.channelCount = 2
 		
+	}
+
+	encodeColors(rgb) {
+
+		if (this._object.connected) {
+			// Function declaration for color data
+			var payload = 'c'
+
+			// All channels on, for now
+			payload += String.fromCharCode(127)
+
+			// Split interpolation time into 3 bytes
+			var interpolationTime = lights.app.splitBytes(7, 3, true)
+
+			payload += String.fromCharCode(interpolationTime[0])
+			payload += String.fromCharCode(interpolationTime[1])
+			payload += String.fromCharCode(interpolationTime[2])
+
+			for (var i = 0; i < this.channelCount * 3; i++) {
+
+				var colorBytes = lights.app.splitBytes(rgb[i] * ((Math.pow(127, 2) - 1 ) / 255), 2, true)
+
+				payload += String.fromCharCode(colorBytes[0])
+				payload += String.fromCharCode(colorBytes[1])
+
+			}
+
+			console.log("Payload length", payload.length)
+			console.log("Payload ", payload)
+
+			console.log(this)
+
+			var call = lights.app.particle.callFunction({ deviceId: this._object.id, name: 'lights', argument: payload, auth: lights.app.particle.auth.accessToken })
+
+			call.then(
+				function(data) {
+					console.log('Function called succesfully:', data);
+				}, function(err) {
+					console.log('An error occurred:', err);
+				})
+
+		} else {
+
+			console.log("Device ", this._object.id, "not connected")
+		}
+
 	}
 
 }
