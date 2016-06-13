5f323413ddfb220e5c38009040a43504101f5c15
Alexander Weiss <ik@alexanderweiss.nl>
2016-06-13 21:48:09 +0200

    Added animation to PagedView and improved layout

--- js/PagedView.js
+++ js/PagedView.js
@@ -83,10 +83,112 @@ class PagedView extends lrs.View {
 	
 	animate(direction, inView, outView) {
 		
-		inView.show()
-		if (outView) outView.hide()
+		var inEls = inView.animationEls
+		var outEls = outView ? outView.animationEls : null
 		
-		return Promise.resolve()
+		var width = this.el.offsetWidth
+		
+		var duration = 650
+		var points = [
+			{ x: 0, y: 0, cp: [{ x: 0.23, y: 1 }] },
+			{ x: 1, y: 1, cp: [{ x: 0.23, y: 1 }] }
+		]
+		
+		var defaultOptions = {
+			type: dynamics.bezier,
+			duration,
+			points
+		}
+		
+		inView.show()
+		
+		// Animate content.
+		var animation = animate(inEls.content).from({
+			
+			translateX: (direction === 'left' ? '100%' : '-100%')
+			
+		}).to({
+			
+			translateX: 0
+			
+		}, defaultOptions)
+		
+		// Animate back button and title.
+		// TODO: Get alignment right.
+		var dX = width / 2
+		animate(inEls.toolbar.backBtn ? [inEls.toolbar.title, inEls.toolbar.backBtn] : inEls.toolbar.title).from({
+			
+			translateX: (direction === 'left' ? dX : -dX),
+			opacity: 0
+			
+		}).to({
+			
+			translateX: 0,
+			opacity: 1
+			
+		}, defaultOptions)
+		
+		// Animate other toolbar items.
+		animate(inEls.toolbar.other).from({
+			
+			opacity: 0
+			
+		}).to({
+			
+			opacity: 1
+			
+		}, defaultOptions)
+		
+		// Animate out, if there's a view to animatie out.
+		if (outView) {
+			
+			// Animate content.
+			animate(outEls.content).to({
+				
+				translateX: (direction === 'left' ? -width / 2 : width)
+				
+			}, defaultOptions).then( function() {
+				
+				outView.hide()
+				
+			})
+			
+			// Animate back button and title.
+			// TODO: Get alignment right.
+			var dX = width / 2
+			animate(outEls.toolbar.backBtn ? [outEls.toolbar.title, outEls.toolbar.backBtn] : outEls.toolbar.title).to({
+				
+				translateX: (direction === 'left' ? -dX : dX),
+				opacity: 0
+				
+			}, defaultOptions)
+			
+			// Animate overlay.
+			;(direction === 'left' ? outEls.content : inEls.content).appendChild(this.overlayEl)
+			animate(this.overlayEl).from({
+				
+				opacity: (direction === 'left' ? 0 : 0.4)
+				
+			}).to({
+				
+				opacity: (direction === 'left' ? 0.4 : 0)
+				
+			}, defaultOptions).then( () => {
+				
+				;(direction === 'left' ? outEls.content : inEls.content).removeChild(this.overlayEl)
+				
+			})
+			
+			// Animate other toolbar items.
+			animate(outEls.toolbar.other).to({
+				
+				opacity: 0
+				
+			}, defaultOptions)
+			
+		}
+		
+		return animation
 		
 	}
 	
@@ -120,6 +222,47 @@ class PageView extends lrs.View {
 			this.views.backBtn.title = title
 			
 		}
+		
+	}
+	
+	get animationEls() {
+		
+		if (!this._animationEls) {
+			
+			Object.defineProperty(this, '_animationEls', {
+				configurable: false,
+				enumerable: false,
+				value: {
+					toolbar: {
+						backBtn: this.views.backBtn ? this.views.backBtn.el : null,
+						title: null,
+						other: []
+					},
+					content: this.el.querySelector('.page-content')
+				}
+			})
+			
+			for (let childEl of this.el.querySelector('.toolbar').children) {
+				
+				if (this.views.backBtn && childEl === this.views.backBtn.el) {
+					
+					continue
+					
+				} else if (childEl.tagName === 'H1') {
+					
+					this._animationEls.toolbar.title = childEl 
+					
+				} else {
+					
+					this._animationEls.toolbar.other.push(childEl)
+					
+				}
+				
+			}
+			
+		}
+		
+		return this._animationEls
 		
 	}
 
